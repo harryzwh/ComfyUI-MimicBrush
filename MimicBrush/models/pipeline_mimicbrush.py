03b7c68ed131f892dd8cc73b34381a54f5d3a66c
harryzwh <harryzwh@126.com>
2024-12-16 16:27:15 +0800

    Update pipeline_mimicbrush.py

--- MimicBrush/models/pipeline_mimicbrush.py
+++ MimicBrush/models/pipeline_mimicbrush.py
@@ -32,7 +32,8 @@ from diffusers.utils.torch_utils import randn_tensor
 from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
-from models.ReferenceNet_attention import ReferenceNetAttention
+# from models.ReferenceNet_attention import ReferenceNetAttention
+from MimicBrush.models.ReferenceNet_attention import ReferenceNetAttention
 
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
